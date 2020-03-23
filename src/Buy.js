6dac601eedd1c7617f6ebd1d3942397d577e4e18
Andrew Young <andrewtateyoung@gmail.com>
2020-03-23 15:58:47 -0500

    boxes for buy page

---
+++ src/Buy.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./css/Buy.css";
+import HeaderBar from "./HeaderBar";
+import Art from "./images/art.jpeg";
+import Books from "./images/book.jpg";
+// import Fashion from "./images/fashion.jpg";
+
+export default class Buy extends React.Component {
+  render() {
+    const categories = [
+      { picture: Art, name: "Art & Home Decoration", link: "art" },
+      { picture: Books, name: "Collectibles", link: "collectibles" },
+      { picture: Books, name: "Books", link: "books" },
+      { picture: Books, name: "Electronics", link: "electronics" },
+      { picture: Books, name: "Health & Beauty", link: "health" },
+      { picture: Books, name: "Home & Garden", link: "garden" },
+      { picture: Books, name: "Fashion", link: "fashion" },
+      { picture: Books, name: "Sporting Goods", link: "sports" },
+      { picture: Books, name: "Toys", link: "toys" }
+    ];
+    return (
+      <div>
+        <div>
+          <HeaderBar />
+        </div>
+        <div
+          style={{
+            marginLeft: "15vw",
+            marginRight: "15vw",
+            marginTop: 30
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ fontSize: 24, fontWeight: 600 }}>
+              Shop by Category
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                width: "70vw",
+                justifyContent: "center"
+              }}
+            >
+              {categories.map((cat, index) => {
+                return (
+                  <div
+                    id={"cat"}
+                    onClick={() => this.goToCategory(cat)}
+                    style={{
+                      width: 220,
+                      marginTop: 30,
+                      height: 250,
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      marginLeft: 10,
+                      marginRight: 10
+                    }}
+                  >
+                    <img
+                      src={cat.picture}
+                      style={{
+                        width: 220,
+                        height: 200,
+                        borderRadius: 5,
+                        overflow: "hidden"
+                      }}
+                    ></img>
+                    <div style={{ marginTop: 10 }}>{cat.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  goToCategory(cat) {
+    window.location.href = cat.link;
+  }
+}
--- src/RenderRoutes.js
+++ src/RenderRoutes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import * as firebase from "firebase";
-import App from "./App";
+import Buy from "./Buy";
 import Sell from "./Sell";
 import OrderKit from "./OrderKit";
 import SellRules from "./SellRules";
@@ -11,7 +11,7 @@ export default class RenderRoutes extends React.Component {
     if (firebase.auth().currentUser) {
       return (
         <Router>
-          <Route path="/" exact={true} render={() => <App />} />
+          <Route path="/" exact={true} render={() => <Buy />} />
           <Route path="/sell" exact={true} render={() => <Sell />} />
           <Route path="/sell/kit" exact={true} render={() => <OrderKit />} />
           <Route
@@ -24,7 +24,7 @@ export default class RenderRoutes extends React.Component {
     } else {
       return (
         <Router>
-          <Route path="/" exact={true} render={() => <App />} />
+          <Route path="/" exact={true} render={() => <Buy />} />
           <Route path="/sell" exact={true} render={() => <Sell />} />
           <Route path="/sell/kit" exact={true} render={() => <OrderKit />} />
           <Route
